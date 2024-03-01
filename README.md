1ec24809f46ba9cba65cd5f20947a6eae1168462
mbilawalzaman <mbilawalzaman@gmail.com>
2024-03-01 17:12:41 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Andre Iguodala
+
+A fun web app made with React.js about the clutchest basketball player, Andre Iguodala.
+
+Live at https://dezhaysun.github.io/react-iguodala/
+
+![image](https://github.com/DeZhaysun/react-iguodala/assets/61562373/95791085-1c01-462d-b771-00b4f3d2c1fc)
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./components/header";
+import Description from "./components/description";
+import Clutch from "./components/clutch";
+import Quote from "./components/quote";
+import Stats from "./components/stats";
+import Navbar from "./components/navbar";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    {/* <Header /> */}
+    <Navbar/>
+    <Description />
+    <Clutch />
+    <Quote />
+    <Stats />
+    <footer>&copy; 2022 Jason Tang</footer>
+  </div>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
